2366704e0ce4e1a24cf09b26496cd91e82e2ee94
Jan Aage Lavik <jan.age.lavik@cern.ch>
2016-06-08 09:40:24 +0200

    settings: allow duplicate requests
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- hepcrawl/settings.py
+++ hepcrawl/settings.py
@@ -29,7 +29,8 @@ NEWSPIDER_MODULE = 'hepcrawl.spiders'
 # user-agent
 USER_AGENT = 'hepcrawl (+http://www.inspirehep.net)'
 
-# DUPEFILTER_CLASS = "scrapy.dupefilters.BaseDupeFilter"
+# Allow duplicate requests
+DUPEFILTER_CLASS = "scrapy.dupefilters.BaseDupeFilter"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS=32
