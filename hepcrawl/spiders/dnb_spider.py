e82ef429a6a76b0b9d61e609767d958a02fb0656
Henrik Vesterinen <henrik.vesterinen@cern.ch>
2016-10-07 10:36:38 +0200

    dnb_spider: some fixes
    * Adds GET request mocking to tests.
    
    * Adds validation.
    
    * Cleans up mixed single/double quotes and makes
      some small PEP8 fixes.
    
    Signed-off-by: Henrik Vesterinen <henrik.vesterinen@cern.ch>

--- hepcrawl/spiders/dnb_spider.py
+++ hepcrawl/spiders/dnb_spider.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, print_function
 
 from scrapy import Request
 from scrapy.spiders import XMLFeedSpider
+from inspire_schemas.api import validate as validate_schema
 
 from ..items import HEPRecord
 from ..loaders import HEPLoader
@@ -28,6 +29,15 @@ class DNBSpider(XMLFeedSpider):
 
     This spider takes DNB metadata records which are stored in an XML file.
 
+    First harvest the OAI interface:
+    url = ("https://services.dnb.de/oai/accessToken~" + access_token +
+    "/repository?verb=ListRecords&metadataPrefix=MARC21-xml&from=2013-12-08"
+    "&until=2014-12-10&set=dnb-all:online:dissertations:sg530")
+
+    You should perhaps fetch also DDC 520 theses, not only 530.
+
+    NOTE: you need the access token
+
     1. The spider will parse the local MARC21XML format file for record data
 
     2. If a link to the original repository splash page exists, parse_node
@@ -40,12 +50,10 @@ class DNBSpider(XMLFeedSpider):
     Example usage:
     .. code-block:: console
 
-        scrapy crawl DNB -a source_file=file://`pwd`/tests/responses/dnb/test_1.xml -s "JSON_OUTPUT_DIR=tmp/"
+        scrapy crawl DNB -a source_file=file://`pwd`/tests/responses/dnb/test_1.xml
 
     Happy crawling!
     """
-
-    # TODO: OAI harvester should fetch also DDC 520 theses, not only 530.
 
     name = 'DNB'
     start_urls = []
@@ -54,8 +62,8 @@ class DNBSpider(XMLFeedSpider):
     download_delay = 5  # Is this a good value and how to make this domain specific?
 
     namespaces = [
-        ("OAI-PMH", "http://www.openarchives.org/OAI/2.0/"),
-        ("slim", "http://www.loc.gov/MARC21/slim"),
+        ('OAI-PMH', 'http://www.openarchives.org/OAI/2.0/'),
+        ('slim', 'http://www.loc.gov/MARC21/slim'),
     ]
 
     def __init__(self, source_file=None, *args, **kwargs):
@@ -72,12 +80,13 @@ class DNBSpider(XMLFeedSpider):
         """ Cleans the affiliation element."""
 
         affiliations_raw = node.xpath(
-            "./slim:datafield[@tag='502']/slim:subfield[@code='a']/text()").extract()
+            './slim:datafield[@tag="502"]/slim:subfield[@code="a"]/text()'
+        ).extract()
         affiliations = []
         for aff_raw in affiliations_raw:
-            arlist = aff_raw.split(",")
-            aff = ",".join([i for i in arlist if not
-                            ("diss" in i.lower() or i.strip().isdigit())])
+            arlist = aff_raw.split(',')
+            aff = ','.join([i for i in arlist if not
+                            ('diss' in i.lower() or i.strip().isdigit())])
             affiliations.append(aff)
 
         return affiliations
@@ -85,14 +94,15 @@ class DNBSpider(XMLFeedSpider):
     def get_authors(self, node):
         """Gets the authors."""
         authors_raw = node.xpath(
-            "./slim:datafield[@tag='100']/slim:subfield[@code='a']/text()").extract()
+            './slim:datafield[@tag="100"]/slim:subfield[@code="a"]/text()'
+        ).extract()
         affiliations = self.get_affiliations(node)
 
         authors = []
         for author in authors_raw:
             authors.append({
                 'raw_name': author,
-                'affiliations': [{"value": aff} for aff in affiliations],
+                'affiliations': [{'value': aff} for aff in affiliations],
             })
 
         return authors
@@ -101,7 +111,9 @@ class DNBSpider(XMLFeedSpider):
     def get_thesis_supervisors(node):
         """Create a structured supervisor dictionary."""
         supervisors_raw = node.xpath(
-            "./slim:datafield[@tag='700'][slim:subfield[@code='e'][contains(text(), 'Betreuer')]]/slim:subfield[@code='a']/text()").extract()
+            './slim:datafield[@tag="700"][slim:subfield[@code="e"]'
+            '[contains(text(), "Betreuer")]]/slim:subfield[@code="a"]/text()'
+        ).extract()
 
         supervisors = []
         for supervisor in supervisors_raw:
@@ -115,18 +127,19 @@ class DNBSpider(XMLFeedSpider):
     def get_urls_in_record(node):
         """Return all the different urls in the xml."""
         urls_in_record = node.xpath(
-            "./slim:datafield[@tag='856']/slim:subfield[@code='u']/text()").extract()
+            './slim:datafield[@tag="856"]/slim:subfield[@code="u"]/text()'
+        ).extract()
         return urls_in_record
 
     @staticmethod
     def find_direct_links(urls_in_record):
-        """Determine if the XML file has a direct link."""
+        """Get fulltext and splash page links from the XML metadata."""
         direct_links = []
         splash_links = []
         for link in urls_in_record:
-            if "pdf" in get_mime_type(link) and "jpg" not in link.lower():
+            if 'pdf' in get_mime_type(link):
                 direct_links.append(link)
-            elif "pdf" not in get_mime_type(link):
+            else:
                 splash_links.append(link)
 
         return direct_links, splash_links
@@ -141,18 +154,18 @@ class DNBSpider(XMLFeedSpider):
         urls_in_record = self.get_urls_in_record(node)
         direct_links, splash_links = self.find_direct_links(urls_in_record)
         if not splash_links:
-            response.meta["urls"] = urls_in_record
-            response.meta["record"] = node.extract()
+            response.meta['urls'] = urls_in_record
+            response.meta['record'] = node.extract()
             if direct_links:
-                response.meta["direct_links"] = direct_links
+                response.meta['direct_links'] = direct_links
             return self.build_item(response)
 
         link = splash_links[0]
         request = Request(link, callback=self.scrape_for_abstract)
-        request.meta["urls"] = urls_in_record
-        request.meta["record"] = node.extract()
+        request.meta['urls'] = urls_in_record
+        request.meta['record'] = node.extract()
         if direct_links:
-            request.meta["direct_links"] = direct_links
+            request.meta['direct_links'] = direct_links
         return request
 
     def scrape_for_abstract(self, response):
@@ -168,55 +181,70 @@ class DNBSpider(XMLFeedSpider):
         page_nr = []
         abstract_raw = []
 
-        if ("publikationen.ub.uni-frankfurt.de" in domain or
-                "http://nbn-resolving.de" in domain):
+        if ('publikationen.ub.uni-frankfurt.de' in domain or
+                'http://nbn-resolving.de' in domain):
             abstract_raw = node.xpath(
-                "//span[@class='abstractFull']/pre/text()").extract()
+                '//span[@class="abstractFull"]/pre/text()').extract()
             page_nr = node.xpath(
-                "//tr[./th[contains(text(), 'Pagenumber')]]/td/text()").extract()
-        elif "hss-opus.ub.ruhr-uni-bochum.de" in domain:
+                '//tr[./th[contains(text(), "Pagenumber")]]/td/text()'
+            ).extract()
+        elif 'hss-opus.ub.ruhr-uni-bochum.de' in domain:
             abstract_raw = node.xpath(
-                "//div[@id='abstract']//li/text()").extract()
-        elif "ediss.uni-goettingen.de" in domain:
+                '//div[@id="abstract"]//li/text()').extract()
+        elif 'ediss.uni-goettingen.de' in domain:
             abstract_raw = node.xpath(
-                "//div[@class='simple-item-view-abstract']/span/text()").extract()
-        elif "hss.ulb.uni-bonn.de" in domain:
-            abstract_raw = node.xpath(".//text()[contains(.,'Zusammenfassung')"
-                                      "or contains(., 'Abstract')]/ancestor::*[self::tr]/descendant::*[position() > 1]/text()").extract()
-        elif "kups.ub.uni-koeln.de" in domain:
+                '//div[@class="simple-item-view-abstract"]/span/text()'
+            ).extract()
+        elif 'hss.ulb.uni-bonn.de' in domain:
             abstract_raw = node.xpath(
-                "//div[@class='ep_summary_content_main']/h2/following-sibling::p/text()").extract()
-        # if "something else" in domain:
-            # abstracts = node.xpath(".//somewhere[@else]")
+                './/text()[contains(.,"Zusammenfassung") or '
+                'contains(., "Abstract")]/ancestor::*[self::tr]/'
+                'descendant::*[position() > 1]/text()').extract()
+        elif 'kups.ub.uni-koeln.de' in domain:
+            abstract_raw = node.xpath(
+                '//div[@class="ep_summary_content_main"]/h2/'
+                'following-sibling::p/text()'
+            ).extract()
+        # if 'something else' in domain:
+            # abstracts = node.xpath('.//somewhere[@else]')
 
         if abstract_raw:
-            response.meta["abstract"] = [
-                " ".join(abstract_raw).replace("\r\n", " ")]
-        response.meta["page_nr"] = page_nr
+            response.meta['abstract'] = [
+                ' '.join(abstract_raw).replace('\r\n', ' ')]
+        response.meta['page_nr'] = page_nr
 
         return self.build_item(response)
 
     def build_item(self, response):
         """Build the final record."""
-        node = get_node(response.meta["record"], self.namespaces)
+        node = get_node(response.meta['record'], self.namespaces)
         record = HEPLoader(item=HEPRecord(), selector=node, response=response)
 
         record.add_value('authors', self.get_authors(node))
-        record.add_xpath('title',
-                         "./slim:datafield[@tag='245']/slim:subfield[@code='a']/text()")
-        record.add_xpath('source',
-                         "./slim:datafield[@tag='264']/slim:subfield[@code='b']/text()")
-        record.add_xpath('date_published',
-                         "./slim:datafield[@tag='264']/slim:subfield[@code='c']/text()")
-        record.add_value('thesis_supervisor',
-                         self.get_thesis_supervisors(node))
         record.add_xpath(
-            'language', "./slim:datafield[@tag='041']/slim:subfield[@code='a']/text()")
+            'title',
+            './slim:datafield[@tag="245"]/slim:subfield[@code="a"]/text()')
+        record.add_xpath(
+            'source',
+            './slim:datafield[@tag="264"]/slim:subfield[@code="b"]/text()')
+        record.add_xpath(
+            'date_published',
+            './slim:datafield[@tag="264"]/slim:subfield[@code="c"]/text()')
+        record.add_value(
+            'thesis_supervisor',
+            self.get_thesis_supervisors(node))
+        record.add_xpath(
+            'language',
+            './slim:datafield[@tag="041"]/slim:subfield[@code="a"]/text()')
         record.add_value('urls', response.meta.get('urls'))
-        record.add_value('file_urls', response.meta.get("direct_links"))
-        record.add_value('abstract', response.meta.get("abstract"))
-        record.add_value('page_nr', response.meta.get("page_nr"))
+        record.add_value('file_urls', response.meta.get('direct_links'))
+        record.add_value('abstract', response.meta.get('abstract'))
+        record.add_value('page_nr', response.meta.get('page_nr'))
 
         record.add_value('thesis', {'degree_type': 'PhD'})
         record.add_value('collections', ['HEP', 'THESIS'])
-        return record.load_item()
+
+        parsed_record = record.load_item()
+        validate_schema(data=dict(parsed_record), schema_name='hep')
+
+        return parsed_record
