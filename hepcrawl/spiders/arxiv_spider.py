453bb7783b458a026e30e661e7c58667df01f453
David Caro <david@dcaro.es>
2016-10-10 13:18:57 +0200

    arxiv_spider: adapt report numbers to schema
    Signed-off-by: David Caro <david@dcaro.es>

--- hepcrawl/spiders/arxiv_spider.py
+++ hepcrawl/spiders/arxiv_spider.py
@@ -82,7 +82,10 @@ class ArxivSpider(XMLFeedSpider):
             self._get_arxiv_report_numbers(node)
         )
 
-        categories = ' '.join(node.xpath('.//categories//text()').extract()).split()
+        plain_categories = ' '.join(
+            node.xpath('.//categories//text()').extract()
+        ).split()
+        categories = self._get_categories_object(plain_categories)
         record.add_value('field_categories', categories)
         record.add_value(
             'arxiv_eprints',
@@ -101,6 +104,16 @@ class ArxivSpider(XMLFeedSpider):
         validate_schema(data=parsed_record, schema_name='hep')
 
         return parsed_record
+
+    def _get_categories_object(self, plain_categories):
+        categories = []
+        for category in plain_categories:
+            categories.append({
+                'source': 'publisher',
+                'term': category,
+                'scheme': 'ARXIV',
+            })
+        return categories
 
     def _get_authors_or_collaboration(self, node):
         """Parse authors, affiliations; extract collaboration"""
