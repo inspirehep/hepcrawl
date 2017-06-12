de133144e44c35cf90fa93c665ad4f10335c0c6b
Spiros Delviniotis <spirosdelviniotis@gmail.com>
2017-06-12 15:03:07 +0200

    tests: minor fix - fixed indentation
    Signed-off-by: Spiros Delviniotis <spirosdelviniotis@gmail.com>

--- tests/unit/test_phil.py
+++ tests/unit/test_phil.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import json
 
 import pytest
+import requests_mock
 
 from hepcrawl.spiders import phil_spider
 
@@ -36,6 +37,7 @@ def record():
     assert parsed_record
     return parsed_record
 
+
 @pytest.fixture
 def journal():
     """Return results generator from the Phil spider.
@@ -48,6 +50,7 @@ def journal():
     response.meta["jsonrecord"] = jsonrecord[0]
     return spider.build_item(response)
 
+
 @pytest.fixture
 def authors():
     """Returns get_authors() output."""
@@ -56,6 +59,7 @@ def authors():
     jsonrecord = json.loads(response.body_as_unicode())
     response.meta["jsonrecord"] = jsonrecord[0]
     return spider.get_authors(jsonrecord[0]['authors'])
+
 
 @pytest.fixture
 def parse_requests():
@@ -67,16 +71,150 @@ def parse_requests():
     response = fake_response_from_file('phil/test_thesis.json')
     return spider.parse(response)
 
+
 @pytest.fixture
 def splash():
     """Returns a call to build_item(), and ultimately the HEPrecord"""
     spider = phil_spider.PhilSpider()
-    response = fake_response_from_file("phil/fake_splash.html", url="http://philpapers.org/rec/SDFGSDFGDGSDF")
+    response = fake_response_from_file(
+        "phil/fake_splash.html",
+        url="http://philpapers.org/rec/SDFGSDFGDGSDF",
+    )
 
     response.meta["urls"] = [u'http://philpapers.org/rec/SDFGSDFGDGSDF']
     response.meta["jsonrecord"] = {
-        u'publisher': u'', u'doi': None, u'links': [u'http://philpapers.org/rec/SDFGSDFGDGSDF'], u'title': u'Bringing Goodness', u'journal': u'', u'type': u'book', u'abstract': u'Now indulgence dissimilar for his thoroughly has terminated. Agreement offending commanded my an. Change wholly say why eldest period. Are projection put celebrated particular unreserved joy unsatiable its. In then dare good am rose bred or. On am in nearer square wanted.', u'ant_publisher': u'', u'year': u'14/12/2015', u'editors': [], u'collection': u'', u'pages': u'', u'volume': u'0', u'pub_type': u'thesis', u'pubInfo': u'Dissertation, The University of Somewhere', u'authors': [u'Jennings, Bob'], u'issue': u'', u'id': u'SDFGSDFGDGSDF', u'categories': [{u'ancestry': [{u'id': u'5680', u'name': u'Philosophy of Physical Science'}, {u'id': u'5719', u'name': u'Philosophy of Cosmology'}, {u'id': u'5731', u'name': u'Design and Observership in Cosmology'}, {u'id': u'5733', u'name': u'Anthropic Principle'}], u'id': u'5733', u'name': u'Anthropic Principle'}, {u'ancestry': [{u'id': u'5856', u'name': u'Philosophy of Probability'}, {u'id': u'5878', u'name': u'Probabilistic Reasoning'}, {u'id': u'5919', u'name': u'Subjective Probability'}, {u'id': u'5927', u'name': u'Imprecise Credences'}], u'id': u'5927', u'name': u'Imprecise Credences'}, {u'ancestry': [{u'id': u'5932', u'name': u'General Philosophy of Science'}, {u'id': u'6100', u'name': u'Theories and Models'}, {u'id': u'6112', u'name': u'Theoretical Virtues'}, {u'id': u'6122', u'name': u'Simplicity and Parsimony'}], u'id': u'6122', u'name': u'Simplicity and Parsimony'}, {u'ancestry': [{u'id': u'5680', u'name': u'Philosophy of Physical Science'}, {u'id': u'5750', u'name': u'Philosophy of Physics, Miscellaneous'}, {u'id': u'5751', u'name': u'Astrophysics'}], u'id': u'5751', u'name': u'Astrophysics'}, {u'ancestry': [{u'id': u'5856', u'name': u'Philosophy of Probability'}, {u'id': u'5878', u'name': u'Probabilistic Reasoning'}, {u'id': u'5879', u'name': u'Bayesian Reasoning'}, {u'id': u'5881', u'name': u'Bayesian Reasoning, Misc'}], u'id': u'5881', u'name': u'Bayesian Reasoning, Misc'}]
-        }
+        u'publisher': u'',
+        u'doi': None,
+        u'links': [
+            u'http://philpapers.org/rec/SDFGSDFGDGSDF'
+        ],
+        u'title': u'Bringing Goodness',
+        u'journal': u'',
+        u'type': u'book',
+        u'abstract': u'Now indulgence dissimilar for his thoroughly has terminated. Agreement '
+                     u'offending commanded my an. Change wholly say why eldest period. Are pro'
+                     u'jection put celebrated particular unreserved joy unsatiable its. In the'
+                     u'n dare good am rose bred or. On am in nearer square wanted.',
+        u'ant_publisher': u'',
+        u'year': u'14/12/2015',
+        u'editors': [],
+        u'collection': u'',
+        u'pages': u'',
+        u'volume': u'0',
+        u'pub_type': u'thesis',
+        u'pubInfo': u'Dissertation, The University of Somewhere',
+        u'authors': [u'Jennings, Bob'],
+        u'issue': u'',
+        u'id': u'SDFGSDFGDGSDF',
+        u'categories': [
+            {
+                u'ancestry': [
+                    {
+                        u'id': u'5680',
+                        u'name': u'Philosophy of Physical Science'
+                    },
+                    {
+                        u'id': u'5719',
+                        u'name': u'Philosophy of Cosmology'
+                    },
+                    {
+                        u'id': u'5731',
+                        u'name': u'Design and Observership in Cosmology'
+                    },
+                    {
+                        u'id': u'5733',
+                        u'name': u'Anthropic Principle'
+                    }
+                ],
+                u'id': u'5733',
+                u'name': u'Anthropic Principle'
+            },
+            {
+                u'ancestry': [
+                    {
+                        u'id': u'5856',
+                        u'name': u'Philosophy of Probability'
+                    },
+                    {
+                        u'id': u'5878',
+                        u'name': u'Probabilistic Reasoning'
+                    },
+                    {
+                        u'id': u'5919',
+                        u'name': u'Subjective Probability'
+                    },
+                    {
+                        u'id': u'5927',
+                        u'name': u'Imprecise Credences'
+                    }
+                ],
+                u'id': u'5927',
+                u'name': u'Imprecise Credences'
+            },
+            {
+                u'ancestry': [
+                    {
+                        u'id': u'5932',
+                        u'name': u'General Philosophy of Science'
+                    },
+                    {
+                        u'id': u'6100',
+                        u'name': u'Theories and Models'
+                    },
+                    {
+                        u'id': u'6112',
+                        u'name': u'Theoretical Virtues'
+                    },
+                    {
+                        u'id': u'6122',
+                        u'name': u'Simplicity and Parsimony'
+                    }
+                ],
+                u'id': u'6122',
+                u'name': u'Simplicity and Parsimony'
+            },
+            {
+                u'ancestry': [
+                    {
+                        u'id': u'5680',
+                        u'name': u'Philosophy of Physical Science'
+                    },
+                    {
+                        u'id': u'5750',
+                        u'name': u'Philosophy of Physics, Miscellaneous'
+                    },
+                    {
+                        u'id': u'5751',
+                        u'name': u'Astrophysics'
+                    }
+                ],
+                u'id': u'5751',
+                u'name': u'Astrophysics'
+            },
+            {
+                u'ancestry': [
+                    {
+                        u'id': u'5856',
+                        u'name': u'Philosophy of Probability'
+                    },
+                    {
+                        u'id': u'5878',
+                        u'name': u'Probabilistic Reasoning'
+                    },
+                    {
+                        u'id': u'5879',
+                        u'name': u'Bayesian Reasoning'
+                    },
+                    {
+                        u'id': u'5881',
+                        u'name': u'Bayesian Reasoning, Misc'
+                    }
+                ],
+                u'id': u'5881',
+                u'name': u'Bayesian Reasoning, Misc'
+            }
+        ]
+    }
 
     return spider.scrape_for_pdf(response)
 
@@ -85,6 +223,7 @@ def test_scrape(splash):
     """Test pdf link scraping"""
     assert splash["file_urls"]
     assert splash["file_urls"] == [u'http://philpapers.org/www.example.com/file.pdf']
+
 
 def test_parse(parse_requests):
     """Test request metadata that has been defined in parse()."""
@@ -103,6 +242,7 @@ def test_abstract(record):
 
     assert 'abstract' in record
     assert record['abstract'] == abstract
+
 
 def test_title(record):
     """Test extracting title."""
@@ -129,6 +269,7 @@ def test__thesis_authors(record):
     for index, name in enumerate(authors):
         assert unicode(record['authors'][index]['raw_name']) == name
 
+
 def test__journal_authors(journal):
     """Test authors."""
     authors = [u"Jennings, Bob", u"Frederik, Jensen"]
@@ -149,6 +290,7 @@ def test_pdf_link(record):
     assert 'file_urls' in record
     assert record['file_urls'] == files
 
+
 def test_journal(journal):
     """Test journal info getting."""
     title = "Analys"
@@ -157,6 +299,7 @@ def test_journal(journal):
     assert journal['journal_title'] == title
     assert journal['journal_volume'] == volume
     assert journal['journal_issue'] == issue
+
 
 def test_get_authors(authors):
     """Test author getting"""
