1b102a18d1d13ed24663b0e64709a9464428eb49
David Caro <david@dcaro.es>
2017-08-23 19:13:58 +0200

    global: upgrade schemas to v46
    Sem-Ver: breaking change
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ readme = open('README.rst').read()
 
 install_requires = [
     'autosemver~=0.2',
-    'inspire-schemas~=42.0',
-    'inspire-dojson~=41.0',
+    'inspire-schemas~=46.0',
+    'inspire-dojson~=46.0',
     'Scrapy>=1.1.0',
     # TODO: unpin once they support wheel building again
     'scrapyd==1.1.0',
