7b1007acc37fb05ff36921fa175ff12c31ce0645
David Caro <david@dcaro.es>
2017-08-24 19:10:25 +0200

    tests.desy: refactor and improve test fixtures
    Signed-off-by: David Caro <david@dcaro.es>

--- tests/functional/desy/test_desy.py
+++ tests/functional/desy/test_desy.py
@@ -11,10 +11,11 @@
 
 from __future__ import absolute_import, division, print_function
 
-import pytest
-
-from time import sleep
+import copy
 import hashlib
+from time import sleep
+
+import pytest
 
 from hepcrawl.testlib.celery_monitor import CeleryMonitor
 from hepcrawl.testlib.fixtures import (
@@ -26,13 +27,39 @@ from hepcrawl.testlib.tasks import app as celery_app
 from hepcrawl.testlib.utils import get_crawler_instance
 
 
-def override_generated_fields(record):
-    record['acquisition_source']['datetime'] = u'2017-04-03T10:26:40.365216'
-    record['acquisition_source']['submission_number'] = (
-        u'5652c7f6190f11e79e8000224dabeaad'
+def override_dynamic_fields_on_records(records):
+    clean_records = []
+    for record in records:
+        clean_record = override_dynamic_fields_on_record(record)
+        clean_records.append(clean_record)
+
+    return clean_records
+
+
+def override_dynamic_fields_on_record(record):
+    def _override(field_key, original_dict, backup_dict, new_value):
+        backup_dict[field_key] = original_dict[field_key]
+        original_dict[field_key] = new_value
+
+    clean_record = copy.deepcopy(record)
+    overriden_fields = {}
+    dummy_random_date = u'2017-04-03T10:26:40.365216'
+
+    overriden_fields['acquisition_source'] = {}
+    _override(
+        field_key='datetime',
+        original_dict=clean_record['acquisition_source'],
+        backup_dict=overriden_fields['acquisition_source'],
+        new_value=dummy_random_date,
+    )
+    _override(
+        field_key='submission_number',
+        original_dict=clean_record['acquisition_source'],
+        backup_dict=overriden_fields['acquisition_source'],
+        new_value=u'5652c7f6190f11e79e8000224dabeaad',
     )
 
-    return record
+    return clean_record
 
 
 def assert_files_equal(file_1, file_2):
@@ -49,34 +76,7 @@ def assert_files_equal(file_1, file_2):
     assert file_1_hash == file_2_hash
 
 
-@pytest.fixture(scope="function")
-def fft_1_path():
-    return get_test_suite_path(
-        'desy',
-        'fixtures',
-        'ftp_server',
-        'DESY',
-        'FFT',
-        'test_fft_1.txt',
-        test_suite='functional',
-    )
-
-
-@pytest.fixture(scope="function")
-def fft_2_path():
-    return get_test_suite_path(
-        'desy',
-        'fixtures',
-        'ftp_server',
-        'DESY',
-        'FFT',
-        'test_fft_2.txt',
-        test_suite='functional',
-    )
-
-
-@pytest.fixture(scope="function")
-def ftp_environment():
+def get_ftp_settings():
     netrc_location = get_test_suite_path(
         'desy',
         'fixtures',
@@ -85,11 +85,7 @@ def ftp_environment():
         test_suite='functional',
     )
 
-    # The test must wait until the docker environment is up (takes about 10
-    # seconds).
-    sleep(10)
-
-    yield {
+    return {
         'CRAWLER_HOST_URL': 'http://scrapyd:6800',
         'CRAWLER_PROJECT': 'hepcrawl',
         'CRAWLER_ARGUMENTS': {
@@ -98,12 +94,8 @@ def ftp_environment():
         }
     }
 
-    clean_dir('/tmp/file_urls')
-    clean_dir('/tmp/DESY')
 
-
-@pytest.fixture(scope="function")
-def local_environment():
+def get_local_settings():
     package_location = get_test_suite_path(
         'desy',
         'fixtures',
@@ -112,7 +104,7 @@ def local_environment():
         test_suite='functional',
     )
 
-    yield {
+    return {
         'CRAWLER_HOST_URL': 'http://scrapyd:6800',
         'CRAWLER_PROJECT': 'hepcrawl',
         'CRAWLER_ARGUMENTS': {
@@ -120,31 +112,51 @@ def local_environment():
         }
     }
 
+
+@pytest.fixture(scope="function")
+def cleanup():
+    # The test must wait until the docker environment is up (takes about 10
+    # seconds).
+    sleep(10)
+    yield
+
     clean_dir('/tmp/file_urls')
     clean_dir('/tmp/DESY')
 
 
 @pytest.mark.parametrize(
-    'expected_results',
+    'expected_results, settings',
     [
-        expected_json_results_from_file(
-            'desy',
-            'fixtures',
-            'desy_ftp_records.json',
+        (
+            expected_json_results_from_file(
+                'desy',
+                'fixtures',
+                'desy_ftp_records_expected.json',
+            ),
+            get_ftp_settings(),
         ),
+        (
+            expected_json_results_from_file(
+                'desy',
+                'fixtures',
+                'desy_local_records_expected.json',
+            ),
+            get_local_settings(),
+        )
+
     ],
     ids=[
-        'smoke',
+        'ftp package',
+        'local package'
     ]
 )
-def test_desy_ftp(
-        ftp_environment,
-        expected_results,
-        fft_1_path,
-        fft_2_path,
+def test_desy(
+    expected_results,
+    settings,
+    cleanup,
 ):
     crawler = get_crawler_instance(
-        ftp_environment.get('CRAWLER_HOST_URL')
+        settings.get('CRAWLER_HOST_URL')
     )
 
     results = CeleryMonitor.do_crawl(
@@ -153,72 +165,41 @@ def test_desy_ftp(
         monitor_iter_limit=100,
         events_limit=2,
         crawler_instance=crawler,
-        project=ftp_environment.get('CRAWLER_PROJECT'),
+        project=settings.get('CRAWLER_PROJECT'),
         spider='desy',
         settings={},
-        **ftp_environment.get('CRAWLER_ARGUMENTS')
+        **settings.get('CRAWLER_ARGUMENTS')
     )
 
-    gotten_results = [override_generated_fields(result) for result in results]
-    expected_results = [
-        override_generated_fields(expected)
-        for expected in expected_results
-    ]
+    results = sorted(results, key=lambda x: x['control_number'])
 
-    assert sorted(gotten_results) == expected_results
+    gotten_results = override_dynamic_fields_on_records(results)
+    expected_results = override_dynamic_fields_on_records(expected_results)
 
-    # Check using MD5 Hash if downloaded files are there.
-    for record in expected_results:
-        fft_file_paths = sorted(record['_fft'])
+    assert gotten_results == expected_results
 
-        assert_files_equal(fft_file_paths[0]['path'], fft_2_path)
-        assert_files_equal(fft_file_paths[1]['path'], fft_1_path)
+    for record in gotten_results:
+        assert_ffts_content_matches_expected(record)
 
 
-@pytest.mark.parametrize(
-    'expected_results',
-    [
-        expected_json_results_from_file(
-            'desy',
-            'fixtures',
-            'desy_local_records.json',
-        ),
-    ],
-    ids=[
-        'smoke',
-    ]
-)
-def test_desy_local_package_path(
-        local_environment,
-        expected_results,
-        fft_1_path,
-        fft_2_path,
-):
-    crawler = get_crawler_instance(local_environment.get('CRAWLER_HOST_URL'))
+def assert_ffts_content_matches_expected(record):
+    for fft_field in record.get('_fft', []):
+        assert_fft_content_matches_expected(fft_field)
 
-    results = CeleryMonitor.do_crawl(
-        app=celery_app,
-        monitor_timeout=5,
-        monitor_iter_limit=100,
-        events_limit=2,
-        crawler_instance=crawler,
-        project=local_environment.get('CRAWLER_PROJECT'),
-        spider='desy',
-        settings={},
-        **local_environment.get('CRAWLER_ARGUMENTS')
+
+def assert_fft_content_matches_expected(fft_field):
+    expected_file_name = get_file_name_from_fft(fft_field)
+    assert_files_equal(expected_file_name, fft_field['path'])
+
+
+def get_file_name_from_fft(fft_field):
+    file_path = get_test_suite_path(
+        'desy',
+        'fixtures',
+        'ftp_server',
+        'DESY',
+        'FFT',
+        fft_field['filename'] + fft_field['format'],
+        test_suite='functional',
     )
-
-    gotten_results = [override_generated_fields(result) for result in results]
-    expected_results = [
-        override_generated_fields(expected)
-        for expected in expected_results
-    ]
-
-    assert sorted(gotten_results) == expected_results
-
-    # Check using MD5 Hash if downloaded files are there.
-    for record in expected_results:
-        fft_file_paths = sorted(record['_fft'])
-
-        assert_files_equal(fft_file_paths[0]['path'], fft_2_path)
-        assert_files_equal(fft_file_paths[1]['path'], fft_1_path)
+    return file_path
--- tests/unit/test_desy.py
+++ tests/unit/test_desy.py
@@ -19,7 +19,10 @@ from scrapy.settings import Settings
 from hepcrawl import settings
 from hepcrawl.pipelines import InspireCeleryPushPipeline
 from hepcrawl.spiders import desy_spider
-from hepcrawl.testlib.fixtures import fake_response_from_file
+from hepcrawl.testlib.fixtures import (
+    expected_json_results_from_file,
+    fake_response_from_file,
+)
 
 
 def create_spider():
@@ -45,7 +48,7 @@ def get_records(response_file_name):
     spider = create_spider()
     records = spider.parse(
         fake_response_from_file(
-            file_name=response_file_name,
+            file_name='desy/' + response_file_name,
             response_type=TextResponse
         )
     )
@@ -67,6 +70,15 @@ def get_one_record(response_file_name):
     return record
 
 
+def get_expected_fixture(response_file_name):
+    expected_record = expected_json_results_from_file(
+        'responses/desy',
+        response_file_name,
+        test_suite='unit',
+    )
+    return expected_record
+
+
 def override_generated_fields(record):
     record['acquisition_source']['datetime'] = '2017-05-04T17:49:07.975168'
     record['acquisition_source']['submission_number'] = (
@@ -77,367 +89,25 @@ def override_generated_fields(record):
 
 
 @pytest.mark.parametrize(
-    'generated_record',
+    'generated_records, expected_records',
     [
-        get_one_record('desy/desy_record.xml'),
+        (
+            [get_one_record('desy_record.xml')],
+            [get_expected_fixture('desy_record_expected.json')],
+        ),
+        (
+            list(get_records('desy_collection_records.xml')),
+            get_expected_fixture('desy_collection_records_expected.json'),
+        ),
     ],
     ids=[
-        'smoke',
+        'single record',
+        'collection of records',
     ]
 )
-def test_pipeline_record(generated_record):
-    expected = {
-        '$schema': 'hep.json',
-        '_fft': [
-            {
-                'creation_datetime': '2017-06-27T09:43:17',
-                'description': (
-                    '00013 Decomposition of the problematic rotation curves '
-                    'in our sample according to the best-fit '
-                    '\\textsc{core}NFW models. Colors and symbols are as in '
-                    'Figure \\ref{fig:dc14_fits}.'
-                ),
-                'filename': 'cNFW_rogue_curves',
-                'format': '.txt',
-                'path': 'FFT/test_fft_1.txt',
-                'type': 'Main',
-                'version': 1,
-            },
-            {
-                'creation_datetime': '2017-06-27T09:43:16',
-                'description': (
-                    '00005 Comparison of the parameters of the best-fit DC14 '
-                    'models to the cosmological halo mass-concentration '
-                    'relation from \\cite{dutton14} (left) and the stellar '
-                    'mass-halo mass relation from \\cite{behroozi13} (right). '
-                    'The error bars correspond to the extremal values of the '
-                    'multidimensional 68\\% confidence region for each fit. '
-                    'The theoretical relations are shown as red lines and '
-                    'their 1$\\sigma$ and 2$\\sigma$ scatter are represented '
-                    'by the dark and light grey bands, respectively. The '
-                    'mass-concentration relation from \\cite{maccio08} and '
-                    'the stellar mass-halo mass relation from '
-                    '\\cite{behroozi13} are also shown as the black dashed '
-                    'lines.'
-                ),
-                'filename': 'scalingRelations_DutBeh_DC14_all_Oh',
-                'format': '.txt',
-                'path': 'FFT/test_fft_2.txt',
-                'type': 'Main',
-                'version': 1
-            }
-        ],
-        'abstracts': [
-            {
-                'source': 'Deutsches Elektronen-Synchrotron',
-                'value': (
-                    'Dielectric laser acceleration of electrons has recently '
-                    'been\n                demonstrated with significantly '
-                    'higher accelerating gradients than other\n             '
-                    '   structure-based linear accelerators. Towards the '
-                    'development of an integrated 1 MeV\n                '
-                    'electron accelerator based on dielectric laser '
-                    'accelerator technologies,\n                development '
-                    'in several relevant technologies is needed. In this '
-                    'work, recent\n                developments on electron '
-                    'sources, bunching, accelerating, focussing, deflecting '
-                    'and\n                laser coupling structures are '
-                    'reported. With an eye to the near future, '
-                    'components\n                required for a 1 MeV kinetic '
-                    'energy tabletop accelerator producing '
-                    'sub-femtosecond\n                electron bunches are '
-                    'outlined.\n            '
-                )
-            }
-        ],
-        'acquisition_source': {
-            'datetime': '2017-05-04T17:49:07.975168',
-            'method': 'hepcrawl',
-            'source': 'desy',
-            'submission_number': '5652c7f6190f11e79e8000224dabeaad'
-        },
-        'control_number': 111111,
-        'document_type': [
-            'article'
-        ],
-        'dois': [
-            {
-                'value': '10.18429/JACoW-IPAC2017-WEYB1'
-            }
-        ],
-        'number_of_pages': 6,
-        'public_notes': [
-            {
-                'value': '*Brief entry*'
-            }
-        ],
-        'publication_info': [
-            {
-                'parent_isbn': '9783954501823'
-            },
-            {
-                'page_end': '2525',
-                'page_start': '2520',
-                'year': 2017
-            }
-        ],
-        'self': {
-            '$ref': 'https://labs.inspirehep.net/api/literature/111111'
-        },
-        'titles': [
-            {
-                'source': 'JACoW',
-                'title': (
-                    'Towards a Fully Integrated Accelerator on a Chip: '
-                    'Dielectric Laser\n                Acceleration (DLA) '
-                    'From the Source to Relativistic Electrons\n            '
-                )
-            }
-        ]
-    }
-
-    assert override_generated_fields(generated_record) == expected
-
-
-@pytest.mark.parametrize(
-    'generated_records',
-    [
-        get_records('desy/desy_collection_records.xml'),
-    ],
-    ids=[
-        'smoke',
+def test_pipeline(generated_records, expected_records):
+    clean_generated_records = [
+        override_generated_fields(generated_record)
+        for generated_record in generated_records
     ]
-)
-def test_pipeline_collection_records(generated_records):
-    expected = [{
-            "acquisition_source": {
-                "source": "desy",
-                "method": "hepcrawl",
-                "submission_number": "5652c7f6190f11e79e8000224dabeaad",
-                "datetime": "2017-05-04T17:49:07.975168"
-            },
-            "_fft": [
-                {
-                    'creation_datetime': '2017-06-27T09:43:17',
-                    'description': (
-                        '00013 Decomposition of the problematic rotation '
-                        'curves in our sample according to the best-fit '
-                        '\\textsc{core}NFW models. Colors and symbols are as '
-                        'in Figure \\ref{fig:dc14_fits}.'
-                    ),
-                    'filename': 'cNFW_rogue_curves',
-                    'format': '.txt',
-                    'path': 'FFT/test_fft_1.txt',
-                    'type': 'Main',
-                    'version': 1,
-                },
-                {
-                    'creation_datetime': '2017-06-27T09:43:16',
-                    'description': (
-                        '00005 Comparison of the parameters of the best-fit '
-                        'DC14 models to the cosmological halo '
-                        'mass-concentration relation from \\cite{dutton14} '
-                        '(left) and the stellar mass-halo mass relation from '
-                        '\\cite{behroozi13} (right). The error bars correspond'
-                        ' to the extremal values of the multidimensional 68\\%'
-                        ' confidence region for each fit. The theoretical '
-                        'relations are shown as red lines and their '
-                        '1$\\sigma$ and 2$\\sigma$ scatter are represented '
-                        'by the dark and light grey bands, respectively. The '
-                        'mass-concentration relation from \\cite{maccio08} '
-                        'and the stellar mass-halo mass relation from '
-                        '\\cite{behroozi13} are also shown as the black '
-                        'dashed lines.'
-                    ),
-                    'filename': 'scalingRelations_DutBeh_DC14_all_Oh',
-                    'format': '.txt',
-                    'path': 'FFT/test_fft_2.txt',
-                    'type': 'Main',
-                    'version': 1
-                }
-            ],
-            "control_number": 111111,
-            "public_notes": [
-                {
-                    "value": "*Brief entry*"
-                }
-            ],
-            "self": {
-                "$ref": "https://labs.inspirehep.net/api/literature/111111"
-            },
-            "number_of_pages": 6,
-            "titles": [
-                {
-                    "source": "JACoW",
-                    "title": (
-                        'Towards a Fully Integrated Accelerator on a Chip: '
-                        'Dielectric Laser\n                Acceleration (DLA) '
-                        'From the Source to Relativistic Electrons'
-                        '\n            '
-                    )
-                }
-            ],
-            "dois": [
-                {
-                    "value": "10.18429/JACoW-IPAC2017-WEYB1"
-                }
-            ],
-            "publication_info": [
-                {
-                    "parent_isbn": "9783954501823"
-                },
-                {
-                    "page_start": "2520",
-                    "page_end": "2525",
-                    "year": 2017
-                }
-            ],
-            "$schema": "hep.json",
-            "document_type": [
-                "article"
-            ],
-            "abstracts": [
-                {
-                    "source": "Deutsches Elektronen-Synchrotron",
-                    "value": (
-                        "Dielectric laser acceleration of electrons has "
-                        "recently been\n                demonstrated with "
-                        "significantly higher accelerating gradients than "
-                        "other\n                structure-based linear "
-                        "accelerators. Towards the development of an "
-                        "integrated 1 MeV\n                electron "
-                        "accelerator based on dielectric laser accelerator "
-                        "technologies,\n                development in "
-                        "several relevant technologies is needed. In this work"
-                        ", recent\n                developments on electron "
-                        "sources, bunching, accelerating, focussing, "
-                        "deflecting and\n                laser coupling "
-                        "structures are reported. With an eye to the near "
-                        "future, components\n                required for a 1 "
-                        "MeV kinetic energy tabletop accelerator producing sub"
-                        "-femtosecond\n                electron bunches are "
-                        "outlined.\n            "
-                    )
-                }
-            ]
-        },
-        {
-            "acquisition_source": {
-                "source": "desy",
-                "method": "hepcrawl",
-                "submission_number": "5652c7f6190f11e79e8000224dabeaad",
-                "datetime": "2017-05-04T17:49:07.975168"
-            },
-            "_fft": [
-                {
-                    'creation_datetime': '2017-06-27T09:43:17',
-                    'description': (
-                        "00013 Decomposition of the problematic rotation "
-                        "curves in our sample according to the best-fit "
-                        "\\textsc{core}NFW models. Colors and symbols are as "
-                        "in Figure \\ref{fig:dc14_fits}."
-                    ),
-                    'filename': 'cNFW_rogue_curves',
-                    'format': '.txt',
-                    'path': 'FFT/test_fft_1.txt',
-                    'type': 'Main',
-                    'version': 1,
-                },
-                {
-                    'creation_datetime': '2017-06-27T09:43:16',
-                    'description': (
-                        '00005 Comparison of the parameters of the best-fit '
-                        'DC14 models to the cosmological halo '
-                        'mass-concentration relation from \\cite{dutton14} '
-                        '(left) and the stellar mass-halo mass relation '
-                        'from \\cite{behroozi13} (right). The error bars '
-                        'correspond to the extremal values of the '
-                        'multidimensional 68\\% confidence region for each '
-                        'fit. The theoretical relations are shown as red '
-                        'lines and their 1$\\sigma$ and 2$\\sigma$ scatter '
-                        'are represented by the dark and light grey bands, '
-                        'respectively. The mass-concentration relation '
-                        'from \\cite{maccio08} and the stellar mass-halo '
-                        'mass relation from \\cite{behroozi13} are also '
-                        'shown as the black dashed lines.'
-                    ),
-                    'filename': 'scalingRelations_DutBeh_DC14_all_Oh',
-                    'format': '.txt',
-                    'path': 'FFT/test_fft_2.txt',
-                    'type': 'Main',
-                    'version': 1
-                }
-            ],
-            "control_number": 222222,
-            "public_notes": [
-                {
-                    "value": "*Brief entry*"
-                }
-            ],
-            "self": {
-                "$ref": "https://labs.inspirehep.net/api/literature/222222"
-            },
-            "number_of_pages": 6,
-            "titles": [
-                {
-                    "source": "JACoW",
-                    "title": (
-                        "Towards a Fully Integrated Accelerator on a Chip: "
-                        "Dielectric Laser\n                Acceleration "
-                        "(DLA) From the Source to Relativistic Electrons"
-                        "\n            "
-                    )
-                }
-            ],
-            "dois": [
-                {
-                    "value": "10.18429/JACoW-IPAC2017-WEYB1"
-                }
-            ],
-            "publication_info": [
-                {
-                    "parent_isbn": "9783954501823"
-                },
-                {
-                    "page_start": "2520",
-                    "page_end": "2525",
-                    "year": 2017
-                }
-            ],
-            "$schema": "hep.json",
-            "document_type": [
-                "article"
-            ],
-            "abstracts": [
-                {
-                    "source": "Deutsches Elektronen-Synchrotron",
-                    "value": (
-                        "Dielectric laser acceleration of electrons has "
-                        "recently been\n                demonstrated with "
-                        "significantly higher accelerating gradients than "
-                        "other\n                structure-based linear "
-                        "accelerators. Towards the development of an "
-                        "integrated 1 MeV\n                electron "
-                        "accelerator based on dielectric laser accelerator "
-                        "technologies,\n                development in "
-                        "several relevant technologies is needed. In this "
-                        "work, recent\n                developments on "
-                        "electron sources, bunching, accelerating, "
-                        "focussing, deflecting and\n                laser "
-                        "coupling structures are reported. With an eye to "
-                        "the near future, components\n                "
-                        "required for a 1 MeV kinetic energy tabletop "
-                        "accelerator producing sub-femtosecond"
-                        "\n                electron bunches are outlined."
-                        "\n            "
-                    )
-                }
-            ]
-        }
-    ]
-
-    generated_results = [
-        override_generated_fields(rec) for rec in generated_records
-    ]
-
-    assert generated_results == expected
+    assert clean_generated_records == expected_records
