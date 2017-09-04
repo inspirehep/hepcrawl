5a358e179f387a94cd34d8eb26fea311fd487d66
David Caro <david@dcaro.es>
2017-09-04 12:55:07 +0200

    middlewares: refactor crawl once middleware
    Signed-off-by: David Caro <david@dcaro.es>

--- hepcrawl/middlewares.py
+++ hepcrawl/middlewares.py
@@ -24,8 +24,6 @@ from hepcrawl.utils import ftp_connection_info
 
 
 class ErrorHandlingMiddleware(object):
-    """Log errors."""
-
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
@@ -35,13 +33,11 @@ class ErrorHandlingMiddleware(object):
 
     def process_spider_exception(self, response, exception, spider):
         """Register the error in the spider and continue."""
-        self.process_exception(response, exception, spider)
+        return self.process_exception(response, exception, spider)
 
     def process_exception(self, request, exception, spider):
         """Register the error in the spider and continue."""
-        if 'errors' not in spider.state:
-            spider.state['errors'] = []
-        spider.state['errors'].append({
+        spider.state.setdefault('errors', []).append({
             'exception': exception,
             'sender': request,
         })
@@ -68,47 +64,28 @@ class HepcrawlCrawlOnceMiddleware(CrawlOnceMiddleware):
 
     By default it does nothing. To avoid crawling a particular page
     multiple times set ``request.meta['crawl_once'] = True``. Other
-    ``request.meta`` keys:
+    ``request.meta`` keys that modify it's behavior:
 
     * ``crawl_once_value`` - a value to store in DB. By default, timestamp
-      is stored for Http/Https requests and last-modified is stored for FTP/File requests.
+        is stored for Http/Https requests and last-modified is stored for
+        FTP/File requests.
     * ``crawl_once_key`` - unique file name is used.
 
     Settings:
 
-    * ``CRAWL_ONCE_ENABLED`` - set it to False to disable middleware.
-      Default is True.
-    * ``CRAWL_ONCE_PATH`` - a path to a folder with crawled requests database.
-      By default ``.scrapy/crawl_once/`` path is used; this folder contains
-      ``<spider_name>.sqlite`` files with databases of seen requests.
-    * ``CRAWL_ONCE_DEFAULT`` - default value for ``crawl_once`` meta key
-      (False by default). When True, all requests are handled by
-      this middleware unless disabled explicitly using
-      ``request.meta['crawl_once'] = False``.
+    * ``CRAWL_ONCE_ENABLED``:set it to False to disable middleware. Default
+        is True.
+    * ``CRAWL_ONCE_PATH``: a path to a folder with crawled requests database.
+        By default ``.scrapy/crawl_once/`` path is used; this folder contains
+        ``<spider_name>.sqlite`` files with databases of seen requests.
+    * ``CRAWL_ONCE_DEFAULT``: default value for ``crawl_once`` meta key (False
+        by default). When True, all requests are handled by this middleware
+        unless disabled explicitly using
+        ``request.meta['crawl_once'] = False``.
+
+
+    For more info see: https://github.com/TeamHG-Memex/scrapy-crawl-once
     """
-
-    @staticmethod
-    def _is_newer(this_timestamp, than_this_timestamp, scheme):
-        if scheme in ['ftp', 'file']:
-            return this_timestamp > than_this_timestamp
-
-    def _has_to_be_crawled(self, request, spider):
-        request_db_key = self._get_key(request)
-
-        if request_db_key not in self.db:
-            return True
-
-        new_request_timestamp = self._get_timestamp(request, spider)
-        parsed_url = urlparse(request.url)
-        if self._is_newer(
-            new_request_timestamp,
-            self.db.get(key=request_db_key),
-            scheme=parsed_url.scheme,
-        ):
-            return True
-
-        return False
-
     def process_request(self, request, spider):
         if not request.meta.get('crawl_once', self.default):
             return
@@ -120,41 +97,57 @@ class HepcrawlCrawlOnceMiddleware(CrawlOnceMiddleware):
             self.stats.inc_value('crawl_once/ignored')
             raise IgnoreRequest()
 
-    @staticmethod
-    def _get_timestamp(request, spider):
-        def _get_ftp_relative_path(url, host):
-            return url.replace(
-                'ftp://{0}/'.format(host),
-                '',
-            )
+    def _has_to_be_crawled(self, request, spider):
+        request_db_key = self._get_key(request)
 
-        def _get_ftp_timestamp(spider, url):
-            ftp_host, params = ftp_connection_info(spider.ftp_host, spider.ftp_netrc)
-            ftp = FTP(
-                host=ftp_host,
-                user=params['ftp_user'],
-                passwd=params['ftp_password'],
-            )
-            return ftp.sendcmd(
-                'MDTM {}'.format(
-                    _get_ftp_relative_path(
-                        url=url,
-                        host=ftp_host
-                    )
-                )
-            )
+        if request_db_key not in self.db:
+            return True
 
-        def _get_file_timestamp(url):
-            file_path = url.replace('file://', '')
-            return os.stat(file_path).st_mtime
+        new_file_timestamp = self._get_timestamp(request, spider)
+        old_file_timestamp = self.db.get(key=request_db_key)
+        return new_file_timestamp > old_file_timestamp
 
+    @classmethod
+    def _get_timestamp(cls, request, spider):
         parsed_url = urlparse(request.url)
         full_url = request.url
         if parsed_url.scheme == 'ftp':
-            last_modified = _get_ftp_timestamp(spider, full_url)
+            last_modified = cls._get_ftp_timestamp(spider, full_url)
         elif parsed_url.scheme == 'file':
-            last_modified = _get_file_timestamp(full_url)
+            last_modified = cls._get_file_timestamp(full_url)
         else:
             last_modified = time.time()
 
         return last_modified
+
+    @classmethod
+    def _get_ftp_timestamp(cls, spider, url):
+        ftp_host, params = ftp_connection_info(
+            spider.ftp_host,
+            spider.ftp_netrc,
+        )
+        ftp = FTP(
+            host=ftp_host,
+            user=params['ftp_user'],
+            passwd=params['ftp_password'],
+        )
+        return ftp.sendcmd(
+            'MDTM {}'.format(
+                cls._get_ftp_relative_path(
+                    url=url,
+                    host=ftp_host
+                )
+            )
+        )
+
+    @staticmethod
+    def _get_ftp_relative_path(url, host):
+        return url.replace(
+            'ftp://{0}/'.format(host),
+            '',
+        )
+
+    @staticmethod
+    def _get_file_timestamp(url):
+        file_path = url.replace('file://', '')
+        return os.stat(file_path).st_mtime
